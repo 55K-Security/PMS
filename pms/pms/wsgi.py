07e3a5f17405e3b30a224557c0685ebc6f65a791
55K-学安全 <nov15_expert@163.com>
2026-03-02 10:12:57 +0800

    Delete pms directory

--- pms/pms/wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI config for PMS project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pms.settings')
-application = get_wsgi_application()
